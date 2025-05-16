22c7cd967b1ee220345a6ad54661e9292ba02d35
david17ddd <ddomdor1711@g.educaand.es>
2025-05-16 17:18:33 +0200

    Add files via upload

---
+++ employee.py
@@ -0,0 +1,7 @@
+from app import db
+
+class Employee(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    position = db.Column(db.String(100))
+    salary = db.Column(db.Float)
